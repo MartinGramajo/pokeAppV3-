4232331e2ed894b7369c7250b1272d357c6c1a18
¨mago¨ <martingramajo08@gmail.com>
2024-07-10 16:19:12 -0300

    agrego el footer

---
+++ src/common/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="text-center mt-auto py-4 bg-dark text-white">
+      <h6 className="fs-12">
+        Rolling Code School © 2024 Todos los derechos reservados
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
